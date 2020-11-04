cc789dbbbd98546a61d76f3df4d39414a6525f65
Praveen Ukkoji <praveenukkoji@gmail.com>
2020-11-05 02:04:05 +0530

    service file updated

--- src/app/service/service.service.ts
+++ src/app/service/service.service.ts
@@ -12,9 +12,10 @@ export class ServiceService {
     private httpClient: HttpClient
   ) { }
 
-  // branch
+  // BRANCH
 
-  getBranches(): Observable<Response>{
+  // get all branches for database
+  getBranch(): Observable<Response>{
     const url = "http://localhost:8000/branch/get/branches/";
 
     var body = {
@@ -30,7 +31,8 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
-  addBranches(branch_name): Observable<Response>{
+  // create branch in database
+  createBranch(branch_name): Observable<Response>{
     const url = "http://localhost:8000/branch/create/branches/";
 
     var body = {
@@ -50,7 +52,10 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
-  deleteBranches(branch_id): Observable<Response>{
+  // update branch in database
+
+  // delete branch in databse
+  deleteBranch(branch_id): Observable<Response>{
     const url = "http://localhost:8000/branch/delete/branches/";
 
     var body = {
@@ -66,9 +71,9 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
-  // subject
+  // SUBJECT
 
-
+  // get all subject
   getSubjects(): Observable<Response>{
     const url = "http://localhost:8000/subject/get/subjects/";
 
@@ -85,7 +90,8 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
-  addSubjects(subject_name, subject_code, branch_id, year, sem): Observable<Response>{
+  // create subject in database
+  createSubjects(subject_name, subject_code, branch_id, year, sem): Observable<Response>{
     const url = "http://localhost:8000/subject/create/subjects/";
 
     var body = {
@@ -109,6 +115,9 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
+  // update subjects
+
+  // delete subjects in database
   deleteSubjects(subject_id): Observable<Response>{
     const url = "http://localhost:8000/subject/delete/subjects/";
 
@@ -125,18 +134,12 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
-  // faculty
-
-  facultyLogin(email, password): Observable<Response>{
-    const url = "http://localhost:8000/user/login/faculties/";
+  // get subjects according to branch
+  getSubjectBranch(branch_id): Observable<Response>{
+    const url = "http://localhost:8000/subject/getBid/subjects/";
 
     var body = {
-      "faculty": [
-        {
-          "email": email,
-          "password": password
-        }
-      ]
+      "subjects": [branch_id]
     }
     
     let httpOptions = {
@@ -148,6 +151,31 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
+  // get subject according to branch and sem
+  getSubjectBranchSem(branch_id, sem): Observable<Response>{
+    const url = "http://localhost:8000/subject/getSem/subjects/";
+
+    var body = {
+      "subjects": [
+        {
+          "branch_id": branch_id,
+          "sem": sem
+        }
+    ]
+    }
+    
+    let httpOptions = {
+      headers: new HttpHeaders({
+       'Content-Type': 'application/json'
+      })
+    };
+
+    return this.httpClient.post<Response>(url, body, httpOptions);
+  }
+
+  // FACULTY
+
+  // get faculty details 
   getFaculty(faculty_id): Observable<Response>{
     const url = "http://localhost:8000/user/get/faculties/";
 
@@ -164,6 +192,7 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
+  // create faculty
   createFaculty(full_name, email, password, branch_id): Observable<Response>{
     const url = "http://localhost:8000/user/create/faculties/";
 
@@ -187,7 +216,7 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
-  // password
+  // update faculty with password
   updateFaculty(faculty_id, full_name, email, password, branch_id): Observable<Response>{
     const url = "http://localhost:8000/user/update/faculties/";
 
@@ -214,6 +243,7 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
+  // update faculty without password
   updateFaculty1(faculty_id, full_name, email, branch_id): Observable<Response>{
     const url = "http://localhost:8000/user/update/faculties/";
 
@@ -239,6 +269,7 @@ export class ServiceService {
     return this.httpClient.post<Response>(url, body, httpOptions);
   }
 
+  // delete faculty in database
   deleteFaculty(faculty_id): Observable<Response>{
     const url = "http://localhost:8000/user/delete/faculties/";
 
@@ -249,6 +280,120 @@ export class ServiceService {
     let httpOptions = {
       headers: new HttpHeaders({
        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.httpClient.post<Response>(url, body, httpOptions);
+  }
+
+  // login faculty
+  facultyLogin(email, password): Observable<Response>{
+    const url = "http://localhost:8000/user/login/faculties/";
+
+    var body = {
+      "faculty": [
+        {
+          "email": email,
+          "password": password
+        }
+      ]
+    }
+    
+    let httpOptions = {
+      headers: new HttpHeaders({
+       'Content-Type': 'application/json'
+      })
+    };
+
+    return this.httpClient.post<Response>(url, body, httpOptions);
+  }
+
+  // DOCUMENT 
+
+  // get all document
+  getDocuments(): Observable<Response>{
+    const url = "http://localhost:8000/document/get/document/";
+
+    var body = {
+      "document": []
+    }
+    
+    let httpOptions = {
+      headers: new HttpHeaders({
+       'Content-Type': 'application/json'
+      })
+    };
+
+    return this.httpClient.post<Response>(url, body, httpOptions);
+  }
+
+  // create document in database
+  createDocument(document_title, _url, subject_id, module_no): Observable<Response>{
+    const url = "http://localhost:8000/document/create/document/";
+
+    var body = {
+      "document": [
+        {
+          "document_title": document_title,
+          "document_url": _url,
+          "subject_id": subject_id,
+          "module": module_no
+        }
+      ]
+    }
+    
+    let httpOptions = {
+      headers: new HttpHeaders({
+       'Content-Type': 'application/json'
+      })
+    };
+
+    return this.httpClient.post<Response>(url, body, httpOptions);
+  }
+
+  // update document
+
+  // delete document in database
+  deleteDocuments(document_id): Observable<Response>{
+    const url = "http://localhost:8000/document/delete/document/";
+
+    var body = {
+      "document": [document_id]
+    }
+    
+    let httpOptions = {
+      headers: new HttpHeaders({
+       'Content-Type': 'application/json'
+      })
+    };
+
+    return this.httpClient.post<Response>(url, body, httpOptions);
+  }
+
+   // upload document in media folder
+   uploadDocument(formData): Observable<Response>{
+    const url = "http://localhost:8000/document/upload/document/";
+    
+    let httpOptions = {
+      headers: new HttpHeaders({
+       
+      })
+    };
+
+    return this.httpClient.post<Response>(url, formData, httpOptions);
+  }
+
+  // get document by subject_id
+  getDocumentsSubject(id): Observable<Response>{
+    const url = "http://localhost:8000/document/getSid/document/";
+
+    var body = {
+      "document": [id]
+    }
+    
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
       })
     };
 
